eea50203c7d91bbe1fbbf6f44406b394ce86b287
17259197 <17259197@DSN1271058239.cfp127aluno.local>
2018-04-04 11:29:25 -0300

    Segunda Versão 04-04 11:29

---
+++ Agenda/src/br/senai/sp/jandira/app/Agenda.java
@@ -0,0 +1,14 @@
+package br.senai.sp.jandira.app;
+
+import br.senai.sp.jandira.dbUtils.Conexao;
+
+public class Agenda {
+
+	public static void main(String[] args) {
+		
+		
+		Conexao.abrirConexao();
+
+	}
+
+}
---
+++ Agenda/src/br/senai/sp/jandira/dbUtils/Conexao.java
@@ -0,0 +1,26 @@
+package br.senai.sp.jandira.dbUtils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexao { 
+	
+	private static Connection con;
+	
+	public static Connection abrirConexao() {
+		con = null;
+		
+		try {
+			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+			String dbURL = "jdbc:ucanaccess://C:/Users/17259197/Documents/Gitinf2mb/inf2mb/agenda.accdb";
+			con = DriverManager.getConnection(dbURL) ;
+			System.out.println("Banco conectado com sucesso");
+		} catch (Exception erro) {
+			System.out.println("A Torre");
+			System.out.println(erro.getMessage());
+		}
+		
+		return con;
+	}
+
+}
