d54d4a40b9a8c82e4a275e00ae78883b89acd597
17259197 <17259197@DSN1271056531.cfp127aluno.local>
2018-05-02 11:18:17 -0300

    Versão Final 02/05

--- Agenda/src/br/senai/sp/jandira/dbUtils/Conexao.java
+++ Agenda/src/br/senai/sp/jandira/dbUtils/Conexao.java
@@ -13,7 +13,7 @@ public class Conexao {
 		try {
 			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
 			//String dbURL = "jdbc:ucanaccess://C:/Users/17259197/Documents/GitInf2m/inf2mb/agenda.accdb";
-			String dbURL = "jdbc:ucanaccess:////10.107.134.23/banco/agenda.accdb";
+			String dbURL = "jdbc:ucanaccess:////10.107.134.03/banco/agenda.accdb";
 			con = DriverManager.getConnection(dbURL) ;
 			System.out.println("Banco conectado com sucesso");
 		} 
